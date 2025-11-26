b735641c5d60c6808a9fd9c38e77c1e0b22c3c1a
Nathan Miller <nmiller217@gmail.com>
2025-11-25 16:35:49 -0800

    some files

---
+++ quickstart/agent.py
@@ -0,0 +1,12 @@
+from agno.agent import Agent
+from agno.models.openrouter import OpenRouter
+
+# Create your Agent
+agent = Agent(
+    model=OpenRouter(id="x-ai/grok-4.1-fast:free"),
+    instructions="You are a helpful AI assistant.",
+    markdown=True,
+)
+
+# Run
+agent.print_response("What is artificial intelligence?", stream=True)

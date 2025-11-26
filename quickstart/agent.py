816395aca18d86c744da064c82ebaeb2e6778279
Nathan Miller <nmiller217@gmail.com>
2025-11-25 17:14:49 -0800

    working agent

--- quickstart/agent.py
+++ quickstart/agent.py
@@ -1,9 +1,14 @@
+import os
+
 from agno.agent import Agent
 from agno.models.openrouter import OpenRouter
 
 # Create your Agent
 agent = Agent(
-    model=OpenRouter(id="x-ai/grok-4.1-fast:free"),
+    model=OpenRouter(
+        id="x-ai/grok-4.1-fast:free",
+        api_key=os.getenv("OPENROUTER_AGNO_API_KEY"),
+    ),
     instructions="You are a helpful AI assistant.",
     markdown=True,
 )
